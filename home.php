ecf69cda994bff70f7a709c68527b73fd168943a
JanakaShamal <47146026+JanakaShamal@users.noreply.github.com>
2019-11-06 21:33:17 +0530

    home page and css

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+	<script src="https://kit.fontawesome.com/d977be036d.js" crossorigin="anonymous"></script>
+	<title>Software Management System</title>
+</head>
+<body>
+<header>
+<div class="headwrapper clearfix">
+<div class="logo">
+	<h1><i class="fab fa-accusoft"></i>CodeLab</h1>
+
+</div>
+<div class="my"><a href=""><i class="fas fa-user-circle"></i></a></div>
+</div>
+<div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a class="#contact" href="#contact">Projects</a>
+    <a href="#about">Contact</a>
+</div>
+</header>	
+
+</div>
+</body>
+</html>
