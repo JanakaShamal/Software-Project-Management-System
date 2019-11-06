d48602b74cef016fd5b2bf18579e7a24b99be91d
vimuth97 <47806934+vimuth97@users.noreply.github.com>
2019-11-06 21:06:34 +0530

    Add files via upload

---
+++ spms.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE spms;
+
+CREATE TABLE admin( 
+    admin_id int AUTO_INCREMENT, 
+    firstname varchar(50), 
+    lastname varchar(50),
+    email varchar(75),
+    pssword varchar(150),
+    PRIMARY KEY(admin_id) 
+);
+
+CREATE TABLE client( 
+    client_id int AUTO_INCREMENT,
+    firstname varchar(50),
+    lastname varchar(50),
+    email varchar(75),
+    pssword varchar(150),
+    contact_no numeric(15),
+    address varchar(300),
+    acc_no numeric(20),
+    PRIMARY KEY(client_id)
+);
+
+
+CREATE TABLE employee( 
+    employee_id int AUTO_INCREMENT, 
+    firstname varchar(50),
+    lastname varchar(50),
+    email varchar(75),
+    contact_no numeric(15),
+    pssword varchar(150),
+    PRIMARY KEY(employee_id)
+);
+
+
+CREATE TABLE project( 
+    project_id int AUTO_INCREMENT,
+    title varchar(75),
+    pro_status varchar(20),
+    progress numeric(3),
+    category varchar(75),
+    keywords varchar(200),
+    PRIMARY KEY(project_id)
+);
+
+
+CREATE TABLE developer( 
+    developer_id int,
+    PRIMARY KEY(developer_id),
+    FOREIGN KEY(developer_id) REFERENCES employee(employee_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE pro_mangr( 
+    pm_id int,
+    PRIMARY KEY(pm_id),
+    FOREIGN KEY(pm_id) REFERENCES employee(employee_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE owns( 
+    client_id int,
+    project_id int,
+    start_date date,
+    due_date date,
+    PRIMARY KEY(client_id,project_id),
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE,
+    FOREIGN KEY(project_id) REFERENCES project(project_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE manages( 
+    pm_id int,
+    project_id int,
+    PRIMARY KEY(pm_id,project_id),
+    FOREIGN KEY(pm_id) REFERENCES pro_mangr(pm_id),
+    FOREIGN KEY(project_id) REFERENCES project(project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE develops( 
+    developer_id int,
+    project_id int,
+    PRIMARY KEY(developer_id,project_id),
+    FOREIGN KEY(developer_id) REFERENCES developer(developer_id),
+    FOREIGN KEY(project_id) REFERENCES project(project_id) ON DELETE CASCADE
+);
+ 
